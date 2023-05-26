028dcfe53ef7ea470bc6d817f1970b7d4a715313
suhao <suhaoxb@gmail.com>
2023-05-27 06:35:57 +0700

    create task

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,12 @@ app.get("/", (req, res) => {
   res.send("home page");
 });
 
+//Create a task
+app.post("/api/tasks", async (req, res) => {
+  console.log(req.body);
+  res.send("Task created");
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
