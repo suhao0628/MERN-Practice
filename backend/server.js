2b0cee1c698b9e5ba1c9cf1c774a028d7e12237d
suhao <suhaoxb@gmail.com>
2023-05-27 07:04:14 +0700

    get all tasks

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/connectDB");
 const mongoose = require("mongoose");
+const Task = require("./model/taskModel");
 
 const app = express();
 
 //Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // const logger = (req, res, next) => {
 //   console.log("Middle ran");
 //   console.log(req.method);
@@ -19,8 +21,24 @@ app.get("/", (req, res) => {
 
 //Create a task
 app.post("/api/tasks", async (req, res) => {
-  console.log(req.body);
-  res.send("Task created");
+  //console.log(req.body);
+  //res.send("Task created");
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+//Get/Read Data/Task
+app.get("/api/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
